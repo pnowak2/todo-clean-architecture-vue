d773d5fd0ced902211f5b40c1d75f7fc498a01f6
Piotr Nowak <p.nowak2@gmail.com>
2019-12-20 22:20:05 +0100

    feat(presentation): add markup of todos

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo" placeholder="What needs to be done?" autofocus />
+    </header>
+
+    <section class="main">
+      <input class="toggle-all" id="toggle-all" type="checkbox" />
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li>
+          <div class="view">
+            <input class="toggle" type="checkbox" />
+            <label>test</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value="test" />
+        </li>
+      </ul>
+    </section>
+
+    <footer class="footer">
+      <span class="todo-count">
+        <strong>3</strong> items left
+      </span>
+      <ul class="filters">
+        <li>
+          <a class="selected" href="#/">All</a>
+        </li>
+        <li>
+          <a href="#/active">Active</a>
+        </li>
+        <li>
+          <a href="#/completed">Completed</a>
+        </li>
+      </ul>
+    </footer>
+  </section>
 </template>
 
 <style>
-@import '../node_modules/todomvc-app-css/index.css';
-@import '../node_modules/todomvc-common/base.css';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "../node_modules/todomvc-app-css/index.css";
+@import "../node_modules/todomvc-common/base.css";
 </style>
